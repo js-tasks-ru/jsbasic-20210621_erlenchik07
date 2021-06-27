2b5bafe11315040ea604460c262434f50d656f78
Irina Golovkova <57095607+Erlenchik@users.noreply.github.com>
2021-06-27 11:02:12 +0300

    Update index.js

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,5 +1,8 @@
 function factorial(n) {
-   let result = 1;
-  for (i = 1; i <= n; i++) { result = result * i; }
+  let result = 1;
+  for (i = 1; i <= n; i++) {
+    result = result * i;
+  }
   console.log(result);
+  alert(result);
 }
