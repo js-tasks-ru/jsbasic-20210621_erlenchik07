d72fb5e38e56f5a73f54b01b38300734e568eaf1
Irina Golovkova <57095607+Erlenchik@users.noreply.github.com>
2021-06-27 16:47:51 +0300

    Update index.js

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,8 @@
 function checkSpam(str) {
-  // ваш код...
+  return str.toLowerCase().includes('1xbet') || str.toLowerCase().includes('xxx');
 }
+
+alert(checkSpam('1XbeT now'));
+alert(checkSpam('free xxxxx'));
+alert(checkSpam('innocent rabbit'));
+
