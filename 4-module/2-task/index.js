7d5a8e3e728adfdb99091961be35ed9a78a1cb14
Irina Golovkova <57095607+Erlenchik@users.noreply.github.com>
2021-07-07 02:15:29 +0300

    Update index.js

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,12 +1,1 @@
 function makeDiagonalRed(table) {
-  for (let rowIndex = 0; rowIndex < table.rows.length; rowIndex++) {
-    let tdCollection = table.rows[rowIndex].cells;
-    for (let td of tdCollection) {
-      if (td.cellIndex == rowIndex) {
-        td.style.backgroundColor = 'red';
-      }
-    }
-  }
-  return table;
-}
-makeDiagonalRed(table);
