cc6e3020705fe2276500f7dbabb9a04d68688805
Irina Golovkova <57095607+Erlenchik@users.noreply.github.com>
2021-07-07 02:06:48 +0300

    Update index.js

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,12 @@
 function getMinMax(str) {
-  // ваш код...
+  let stringsArr = str.replace(/,/g, " ")
+    .split(" ")
+    .filter(item => parseFloat(item));
+  let maxNumber = Math.max(...stringsArr);
+  let minNumber = Math.min(...stringsArr);
+  return {
+    min: minNumber,
+    max: maxNumber
+  };
 }
+console.log(getMinMax(inputData));
