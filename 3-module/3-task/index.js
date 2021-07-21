88c49f9542c442e3bf275e029b83155cebc49cea
Irina Golovkova <57095607+Erlenchik@users.noreply.github.com>
2021-07-21 20:18:23 +0300

    Update index.js

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -2,10 +2,7 @@ function camelize(str) {
   return str
     .split('-')
     .map((text, index) => {
-      if (index !== 0) {
-        text = text[0].toUpperCase() + text.slice(1);
-      }
-      return text;
+      return index ? text[0].toUpperCase() + text.slice(1) : text;
     })
     .join('');
 }
